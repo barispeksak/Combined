bfa230ee75c61ec0521627a0a8cfc6753a28cb6d
Barış Peksak <barispeksak@icloud.com>
2024-05-06 01:32:03 +0300

    Upload Page and Navigation Bar

---
+++ app/src/main/java/com/example/combineddemo/AddPhotoFragment.java
@@ -0,0 +1,179 @@
+package com.example.combineddemo;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.combineddemo.databinding.FragmentAddPhotoBinding;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AddPhotoFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AddPhotoFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    //private FirebaseStorage firebaseStorage;
+    private FirebaseAuth auth;
+    //private FirebaseFirestore firebaseFirestore;
+
+    //private StorageReference storageReference;
+    Uri imageData;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    ActivityResultLauncher<String> permissionLauncher;
+    private FragmentAddPhotoBinding binding;
+
+    public AddPhotoFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AddPhotoFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AddPhotoFragment newInstance(String param1, String param2) {
+        AddPhotoFragment fragment = new AddPhotoFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        registerLauncher();
+
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentAddPhotoBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+    public void uploadButtonClicked()
+    {
+
+    }
+
+
+
+
+    public void selectedImage(View view)
+    {
+        if(ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.READ_MEDIA_AUDIO) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.READ_MEDIA_VIDEO) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{android.Manifest.permission.READ_MEDIA_IMAGES, android.Manifest.permission.READ_MEDIA_AUDIO, android.Manifest.permission.READ_MEDIA_VIDEO}, 1);
+
+            Snackbar.make(view, "Permission needed for Gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permissiom", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //Ask permission,
+                    permissionLauncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES);
+                    permissionLauncher.launch(android.Manifest.permission.READ_MEDIA_AUDIO);
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_VIDEO);
+                }
+            }).show();
+        }
+        else
+        {
+            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            activityResultLauncher.launch(intentToGallery);
+        }
+    }
+
+    public void registerLauncher()
+    {
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+
+                if(result.getResultCode() == Activity.RESULT_OK)
+                {
+                    Intent intentFromResult = result.getData();
+                    if(intentFromResult != null)
+                    {
+                        imageData = intentFromResult.getData();
+                        binding.imageView.setImageURI(imageData);
+                    }
+                }
+            }
+        });
+
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+
+                if(result)
+                {
+                    Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    activityResultLauncher.launch(intentToGallery);
+                }
+                else
+                {
+                    Toast.makeText(requireContext(), "Permission needed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if(result)
+                {
+                    Intent intentToGallery = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    activityResultLauncher.launch(intentToGallery);
+                }
+                else
+                {
+                    Toast.makeText(requireContext(), "Permission needed", Toast.LENGTH_LONG ).show();
+                }
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/combineddemo/MainPage.java
+++ app/src/main/java/com/example/combineddemo/MainPage.java
@@ -1,14 +1,75 @@
 package com.example.combineddemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.example.combineddemo.databinding.ActivityMainBinding;
+import com.example.combineddemo.databinding.ActivityMainPageBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainPage extends AppCompatActivity {
 
+
+    ActivityMainPageBinding binding;
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
+        binding = ActivityMainPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        frameLayout = findViewById(R.id.frame_layout);
+
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemID = item.getItemId();
+
+                if(itemID == R.id.home) {
+                    replaceFragment(new HomeFragment(), false);
+                } else if (itemID == R.id.add) {
+                    replaceFragment(new AddPhotoFragment(), false);
+                } else if(itemID == R.id.profile) {
+                    replaceFragment(new ProfieFragment(), false);
+                } else {
+                    replaceFragment(new RankingFragment(), false);
+                }
+
+                return true;
+            }
+        });
+
+
+
+    }
+
+
+    private void replaceFragment(Fragment fragment, boolean isAppInitialized)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if(isAppInitialized)
+        {
+            fragmentTransaction.add(R.id.frame_layout, fragment);
+        }
+        else
+        {
+            fragmentTransaction.replace(R.id.frame_layout,fragment);
+        }
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
