1506622942cf1aa5ba7a7a6f755de915f5bbc62b
Barış Peksak <barispeksak@icloud.com>
2024-05-08 21:33:01 +0300

    Upload Photo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,6 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-storage")
+    implementation("com.google.firebase:firebase-firestore")
 }
--- app/src/main/java/com/example/combineddemo/AddPhotoFragment.java
+++ app/src/main/java/com/example/combineddemo/AddPhotoFragment.java
@@ -4,13 +4,17 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -19,12 +23,26 @@ import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.combineddemo.databinding.FragmentAddPhotoBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.UUID;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -42,15 +60,23 @@ public class AddPhotoFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    //private FirebaseStorage firebaseStorage;
+
     private FirebaseAuth auth;
+    private FirebaseStorage firebaseStorage;
+
+    private FirebaseFirestore firebaseFirestore;
+
     //private FirebaseFirestore firebaseFirestore;
 
     //private StorageReference storageReference;
     Uri imageData;
+
+    Bitmap selectedImage;
     ActivityResultLauncher<Intent> activityResultLauncher;
     ActivityResultLauncher<String> permissionLauncher;
     private FragmentAddPhotoBinding binding;
+
+    private StorageReference storageReference;
 
     public AddPhotoFragment() {
         // Required empty public constructor
@@ -78,7 +104,7 @@ public class AddPhotoFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        registerLauncher();
+
 
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
@@ -90,8 +116,14 @@ public class AddPhotoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+
         binding = FragmentAddPhotoBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+        firebaseStorage = FirebaseStorage.getInstance();
+        auth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        storageReference = firebaseStorage.getReference();
+        registerLauncher();
         // Inflate the layout for this fragment
         return view;
     }
@@ -126,88 +158,122 @@ public class AddPhotoFragment extends Fragment {
             }
         });
 
-    }
-
-    public void uploadButtonClicked()
-    {
-
-    }
-
-
-
-    /*public void selectedImage(View view)
-    {
-        if(ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.READ_MEDIA_AUDIO) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.READ_MEDIA_VIDEO) != PackageManager.PERMISSION_GRANTED)
-        {
-            ActivityCompat.requestPermissions(requireActivity(), new String[]{android.Manifest.permission.READ_MEDIA_IMAGES, android.Manifest.permission.READ_MEDIA_AUDIO, android.Manifest.permission.READ_MEDIA_VIDEO}, 1);
-
-            Snackbar.make(view, "Permission needed for Gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permissiom", new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    //Ask permission,
-                    permissionLauncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES);
-                    permissionLauncher.launch(android.Manifest.permission.READ_MEDIA_AUDIO);
-                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_VIDEO);
-                }
-            }).show();
-        }
-        else
-        {
-            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            activityResultLauncher.launch(intentToGallery);
-        }
-    }
-
-     */
-
-    public void registerLauncher()
-    {
-        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        Button button = view.findViewById(R.id.uploadButtonClicked);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onActivityResult(ActivityResult result) {
+            public void onClick(View view) {
+                if (imageData != null) {
 
-                if(result.getResultCode() == Activity.RESULT_OK)
-                {
-                    Intent intentFromResult = result.getData();
-                    if(intentFromResult != null)
-                    {
-                        imageData = intentFromResult.getData();
-                        binding.imageView.setImageURI(imageData);
+                    //universal unique id
+                    UUID uuid = UUID.randomUUID();
+                    final String imageName = "images/" + uuid + ".jpg";
+
+                    storageReference.child(imageName).putFile(imageData).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            //Download URL
+
+                            StorageReference newReference = FirebaseStorage.getInstance().getReference(imageName);
+                            newReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+
+                                    String downloadUrl = uri.toString();
+
+                                    FirebaseUser firebaseUser = auth.getCurrentUser();
+                                    String userEmail = firebaseUser.getEmail();
+
+                                    String comment = binding.commentText.getText().toString();
+
+                                    HashMap<String, Object> postData = new HashMap<>();
+                                    postData.put("useremail",userEmail);
+                                    postData.put("downloadurl",downloadUrl);
+                                    postData.put("comment",comment);
+                                    postData.put("date", FieldValue.serverTimestamp());
+
+                                    firebaseFirestore.collection("Posts").add(postData).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                        @Override
+                                        public void onSuccess(DocumentReference documentReference) {
+
+                                            Intent intent = new Intent(requireContext(), HomeFragment.class);
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                            startActivity(intent);
+
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(requireContext(),e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
+                                        }
+                                    });
+                                }
+                            });
+
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(requireContext(),e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+            }
+        });
+
+
+
+    }
+
+    public void registerLauncher() {
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent intentFromResult = result.getData();
+                            if (intentFromResult != null) {
+                                imageData = intentFromResult.getData();
+                                try {
+
+                                    if (Build.VERSION.SDK_INT >= 28) {
+                                        ImageDecoder.Source source = ImageDecoder.createSource(requireContext().getContentResolver(),imageData);
+                                        selectedImage = ImageDecoder.decodeBitmap(source);
+                                        binding.imageView.setImageBitmap(selectedImage);
+
+                                    } else {
+                                        selectedImage = MediaStore.Images.Media.getBitmap(requireContext().getContentResolver(),imageData);
+                                        binding.imageView.setImageBitmap(selectedImage);
+                                    }
+
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                        }
                     }
-                }
-            }
-        });
+                });
 
-        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
-            @Override
-            public void onActivityResult(Boolean result) {
 
-                if(result)
-                {
-                    Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    activityResultLauncher.launch(intentToGallery);
-                }
-                else
-                {
-                    Toast.makeText(requireContext(), "Permission needed", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+        permissionLauncher =
+                registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+                    @Override
+                    public void onActivityResult(Boolean result) {
+                        if(result) {
+                            //permission granted
+                            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            activityResultLauncher.launch(intentToGallery);
 
-        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
-            @Override
-            public void onActivityResult(Boolean result) {
-                if(result)
-                {
-                    Intent intentToGallery = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    activityResultLauncher.launch(intentToGallery);
-                }
-                else
-                {
-                    Toast.makeText(requireContext(), "Permission needed", Toast.LENGTH_LONG ).show();
-                }
-            }
-        });
+                        } else {
+                            //permission denied
+                            Toast.makeText(requireContext(),"Permisson needed!",Toast.LENGTH_LONG).show();
+                        }
+                    }
 
+                });
     }
 }
