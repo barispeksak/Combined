6ad4ff3507bf98e2f8e56eb8c8098b8148867a7b
Barış Peksak <barispeksak@icloud.com>
2024-05-13 17:31:07 +0300

    Main Page Functinalities

--- app/src/main/java/com/example/combineddemo/HomeFragment.java
+++ app/src/main/java/com/example/combineddemo/HomeFragment.java
@@ -1,64 +1,78 @@
 package com.example.combineddemo;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.combineddemo.home.ModelPost;
+import com.example.combineddemo.home.PostAdapter;
+
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
+    RecyclerView recyclerView;
+    ArrayList<ModelPost> dataList;
+    PostAdapter adapter;
+    ModelPost androidData;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
+    public static HomeFragment newInstance(String param1,String param2)
+    {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString("param1", param1);
+        args.putString("param2", param2);
         fragment.setArguments(args);
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        dataList = ModelPost.getPostData();
+        adapter = new PostAdapter(this.getContext(),dataList);
+        RecyclerView recyclerView = view.findViewById(R.id.postrecyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        recyclerView.setAdapter(adapter);
+        LinearSnapHelper snapHelper = new LinearSnapOneItem();
+        snapHelper.attachToRecyclerView(recyclerView);
+    }
+}
+class LinearSnapOneItem extends LinearSnapHelper
+{
+    public int findTargetSnapPosition(RecyclerView.LayoutManager layoutManager, int X, int Y)
+    {
+        View snapView = findSnapView(layoutManager);
+        int currentPosition = layoutManager.getPosition(snapView);
+        LinearLayoutManager layoutManager1 = (LinearLayoutManager) layoutManager;
+        int position1 = layoutManager1.findFirstVisibleItemPosition();
+        int position2 = layoutManager1.findLastVisibleItemPosition();
+        if(layoutManager.canScrollVertically())
+        {
+            if (X > 500) {
+                currentPosition = position2;
+            } else if (X < 500) {
+                currentPosition = position1;
+            }
+        }
+        return currentPosition;
     }
 }
